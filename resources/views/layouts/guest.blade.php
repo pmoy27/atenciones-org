38437443b5ae570bb53c52a3339c2fa589e39fd6
pmoy27 <pablo.oyarzok@gmail.com>
2024-05-11 20:24:25 -0400

    logo login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -20,7 +20,7 @@
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div>
             <a href="/">
-                <img class="w-[300px] h-[230px]" src="{{ asset('imagenes/logo.png') }}" alt="Logo">
+                <img class="w-[300px] h-[230px]" src="{{ url('/menu/Logo.png') }}" alt="Logo">
 
             </a>
         </div>
